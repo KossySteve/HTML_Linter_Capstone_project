7d700e38af5c72551ec702978d7a99b856205e21
KossySteve <kossyeze@gmail.com>
2020-05-29 00:50:39 +0100

    fixed stickler errors

--- lib/error_checks.rb
+++ lib/error_checks.rb
@@ -64,15 +64,15 @@ module Checks
 
   def check_head_tag_contents(file)
     error_statement = ''
-    head_contents = ['<title>','<meta', '<link','<style>']
-    file_str = file.gsub(/\n|\t/, "")
-    upper_limit = file_str =~ (/<head>/)
-    lower_limit = file_str =~ (/<\/head>/)
+    head_contents = ['<title>', '<meta', '<link', '<style>']
+    file_str = file.gsub(/\n|\t/, '')
+    upper_limit = file_str =~ /<head>/
+    lower_limit = file_str =~ /<\/head>/
 
     head_contents.each do |tag|
       if file_str.include?(tag)
         unless (upper_limit..lower_limit).include? (file_str.index(tag))
-           error_statement << "place #{tag} in between <head></head>"
+          error_statement << "place #{tag} in between <head></head>"
         end
       end
     end
